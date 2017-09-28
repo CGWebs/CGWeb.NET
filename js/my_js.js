a3b7de1fb3e53727e08b8c29ba922044af246889
CGWebs <dariusclark@cgwebdesigns.com>
2017-09-28 09:02:16 -0500

    Fixed Small Screen Sizes
    Fixed Small Screen Sizes for the most part. Still need to figure out the menu button for iPhones.
    
    Also on large screen sizes the "about-us" is cut off where our names are.

---
+++ js/my_js.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+$(function() {
+  
+  // contact form animations
+  $('#contactbutton').click(function() {
+    $('#contactForm').fadeToggle();
+  })
+  $(document).mouseup(function (e) {
+    var container = $("#contactForm");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.fadeOut();
+    }
+  });
+  
+});
